f87e0ea3dc039a5c75d14d22fab2ac81ae6479f0
Moaaz Sidat <moaaz.omer@gmail.com>
2017-11-07 15:41:06 -0800

    works with ssr

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App1 from './packages/app-1'
 import App2 from './packages/app-2'
+import './index.css'
 
 const App = () => (
-  <div style={{fontFamily: 'sans-serif', border: '1px solid red', padding: '0.5em', width: '300px'}}>
+  <div className="root-div">
     Root app, react version: {React.version}
     <App1/>
     <App2/>
